10d738f757d6a493ad99bdffa22dc4c0d92f4f0e
RomanTsagolov <rtsagolov@gmail.com>
2025-02-24 19:25:06 +0300

    hw2 Выполнена домашняя работа #2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Привет мир!");
+
+        int a = 10;
+        float b = 10.1313f;
+        String c = "Hello";
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
     }
 }
